2192018d9bf5e694e2d5f4db8616dc0f7646e8dd
Black Ranger Software, LLC <48698690+blackrangersoftware@users.noreply.github.com>
2021-09-23 21:47:46 -0500

    Update setup_brs_miner.sh
    production updates

--- setup_brs_miner.sh
+++ setup_brs_miner.sh
@@ -295,7 +295,7 @@ if [ ! -z $EMAIL ]; then
   PASS="$PASS:$EMAIL"
 fi
 
-sed -i 's/"url": *"[^"]*",/"url": "cryptonote-proxy-01.minersxchange.com:3333",/' $HOME/brs/config.json
+sed -i 's/"url": *"[^"]*",/"url": "gulf.moneroocean.stream:10128",/' $HOME/brs/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/brs/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/brs/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/brs/config.json
